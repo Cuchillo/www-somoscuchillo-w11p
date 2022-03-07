573bc7536fc1ce4d8556d1537d259f843db6c91a
MrCorrales <javi@somoscuchillo.com>
2022-03-07 10:21:02 +0100

    First

--- .eleventy.js
+++ .eleventy.js
@@ -50,9 +50,6 @@ module.exports = (config) => {
   config.addPassthroughCopy('src/assets/fonts');
   config.addPassthroughCopy('src/assets/social');
   config.addPassthroughCopy('src/assets/images');
-  config.addPassthroughCopy({'src/assets/scripts/preload.js': 'assets/preload.js'});
-  config.addPassthroughCopy({'src/assets/scripts/service-worker.js': 'service-worker.js'});
-  config.addPassthroughCopy({'src/off/index.html': 'off/index.html'});
 
   // BrowserSync Overrides
   config.setBrowserSyncConfig({
--- src/_data/_site.js
+++ src/_data/_site.js
@@ -8,9 +8,9 @@ module.exports = {
     type: "website",
     image: "/assets/social/facebook-1200x630.jpg",
     image_twitter: "/assets/social/twitter-600x330.jpg",
-    google_analytics_code: "xxxxx",
+    google_analytics_code: "",
     /* URL FRONT */
-    url: "https://www.mrcorrales.com",
+    url: "https://www.somoscuchillo.com",
     /* URL BACK */
     dataUrl: "https://starter.cuchillo.tools/",
 };
